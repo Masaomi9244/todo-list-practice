bb33cda1feb2f9c7d75f3fa0c074efb7a27fcb78
Masaomi9244 <mario8masaomiomi@gmail.com>
2022-05-02 18:58:11 +0900

    環境構築（Next.js、tailwindcss）

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import "tailwindcss/tailwind.css";
+import type { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <div className="text-red-500">test</div>
+}
+
+export default MyApp
