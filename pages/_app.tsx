6a7023d96e63a2d35c2acd7cce2c5383806fb330
Masaomi9244 <mario8masaomiomi@gmail.com>
2022-05-02 19:26:14 +0900

    スタイリング

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import "tailwindcss/tailwind.css";
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <div className="text-red-500">test</div>
+  return <Component {...pageProps} />
 }
-
 export default MyApp
