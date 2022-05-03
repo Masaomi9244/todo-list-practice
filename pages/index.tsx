ceaf129ac45a73776f92d546a028d0ea067745b1
Masaomi9244 <mario8masaomiomi@gmail.com>
2022-05-03 23:40:15 +0900

    ロジック部分の作成（リスト部分）

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,28 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const [toDoList, setToDoList] = useState([
+    { id: Math.random(), label: "TODO1", isDone: false },
+    { id: Math.random(), label: "TODO2", isDone: false },
+    { id: Math.random(), label: "TODO3", isDone: true  },
+  ]);
+
+  const toggle = (e: any) => {
+    console.log("aaa")
+    setToDoList((prevToDoList) => {
+      return prevToDoList.map(todo => {
+        if (todo.id === Number(e.target.value)) {
+          return { ...todo, isDone: !todo.isDone};
+        }
+        return todo;
+      });
+    });
+  };
+
   return (
     <div className="w-96 mx-auto p-20">
       <h1 className="text-xl font-bold">ToDo</h1>
@@ -12,24 +31,14 @@ const Home: NextPage = () => {
         <button className="border border-black flex-shrink-0">追加</button>
       </div>
       <ul className="mt-4 space-y-2">
-        <li>
+        {toDoList.map((todo) => (
+          <li key={(todo.id)}>
           <label className="flex items-center gap-x-2">
-            <input type="checkbox" />
-            <span>ToDo1</span>
+            <input type="checkbox" value={todo.id} checked={todo.isDone} onChange={toggle} />
+            <span>{todo.label}</span>
           </label>
         </li>
-        <li>
-          <label className="flex items-center gap-x-2">
-            <input type="checkbox" />
-            <span>ToDo2</span>
-          </label>
-        </li>
-        <li>
-          <label className="flex items-center gap-x-2">
-            <input type="checkbox" />
-            <span>ToDo3</span>
-          </label>
-        </li>
+        ))}
       </ul>
     </div>
   );
